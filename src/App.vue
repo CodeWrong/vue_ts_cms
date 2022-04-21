06d136ff87591f50f7641edc5a492219420e2f19
“Roly” <493782517@qq.com>
2022-04-21 21:25:50 +0800

    chore: init code prettier

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App'
-})
+});
 </script>
 
 <style lang="less">

8b344837332bbeae58f494347f3df7e104f8ab77
“Roly” <493782517@qq.com>
2022-04-21 21:01:54 +0800

    feat: 添加vue-router & vuex 路由以及状态管理工具

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="app">
+    {{ $store.state.name }}
+    <router-link to="/login"></router-link>
+    <router-link to="/main"></router-link>
+    <router-view></router-view>
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  name: 'App'
+})
 </script>
 
 <style lang="less">
